63f26556809385f832f0d2714cb6e5665a4001ce
Marcel Cutts <me@marcelcutts.com>
2018-02-07 15:36:55 +0000

    Provide headers with sendIntrospectionQuery (#21)
    * Add ability to pass headers when introspecting GraphQL services
    
    * Add yargs as a dependency
    
    * Change 'request' to 'query' for consistent language
    
    * Add additional example

--- sendIntrospectionQuery.js
+++ sendIntrospectionQuery.js
@@ -1,8 +1,33 @@
 #!/usr/bin/env node
+var argv = require("yargs")
+  .usage('Usage: $0 <url>  [--headers "key:value"]')
+  .command("url", "URL of the GraphQL endpoint", { alias: "url" })
+  .required(1, "URL is required")
+  .option("H", {
+    alias: "headers",
+    describe: "Additional Headers to send with introspection query",
+    type: "array",
+    coerce: arg => {
+      let additionalHeaders = {};
+      for (const header of arg) {
+        const separator = header.indexOf(":");
+        const name = header.substring(0, separator).trim();
+        const value = header.substring(separator + 1).trim();
+        if (!(name && value)) {
+          throw new Error('Headers should be specified as "Name: Value"');
+        }
+        additionalHeaders[name] = value;
+      }
+      return additionalHeaders;
+    }
+  })
+  .help("?")
+  .alias("?", "help")
+  .example("$0 https://example.com/graphql", "Get GraphQL Schema")
+  .example(`$0 https://example.com/graphql --headers "Authorisation: <token>"`, "Get GraphQL Schema with Authorisation header").argv;
 
-var request = require('request');
-var fs = require('fs');
-
+var request = require("request");
+var fs = require("fs");
 var introspectionQuery = `
 query IntrospectionQuery {
     __schema {
@@ -93,26 +118,24 @@ query IntrospectionQuery {
     }
   }`;
 
-if (process.argv.length !== 3) {
-    console.error(`Usage: ${process.argv[1]} <API_URL>`);
+const requestOptions = {
+  json: true,
+  body: { query: introspectionQuery },
+  headers: { "user-agent": "node.js", ...argv.headers }
+};
+
+request.post(argv._[0], requestOptions, function(error, response, body) {
+  if (error) {
+    console.error("Could not send introspection query: ", error);
     process.exit(1);
-}
+  }
 
-request.post(
-    process.argv[2],
-    { method: 'POST', json: true, body: { query: introspectionQuery } },
-    function (error, response, body) {
-        if (error) {
-            console.error('Could not send introspection query: ', error);
-            process.exit(1);
-        }
+  if (response.statusCode !== 200) {
+    console.error("Non-ok status code from API: ", response.statusCode, response.statusMessage);
+    process.exit(1);
+  }
 
-        if (response.statusCode !== 200) {
-            console.error('Non-ok status code from API: ', response.statusCode, response.statusMessage);
-            process.exit(1);
-        }
+  var result = JSON.stringify(body, null, 2);
 
-        var result = JSON.stringify(body, null, 2);
-
-        fs.writeFileSync('graphql_schema.json', result, { encoding: 'utf-8' });
-    });
+  fs.writeFileSync("graphql_schema.json", result, { encoding: "utf-8" });
+});
